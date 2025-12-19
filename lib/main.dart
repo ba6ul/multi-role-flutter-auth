8406526937f97f175998fbb27e8eda21e28a6818
Babul Raj Singh <95072470+ba6ul@users.noreply.github.com>
2025-12-20 03:13:00 +0530

    main changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:multi_role_flutter_auth/core/config/api_keys.dart';
 import 'package:multi_role_flutter_auth/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:multi_role_flutter_auth/features/init_dependencies.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+
 
 // Pages
 import 'package:multi_role_flutter_auth/features/auth/presentation/pages/login_screen.dart';
